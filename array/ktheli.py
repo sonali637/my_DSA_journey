66a8bcea8d994eb613273933c90a90de4b3d5fad
sonali637 <sonalibiswal108@gmail.com>
2026-06-29 13:58:07 +0530

    kth 2nd largest number

--- array/ktheli.py
+++ array/ktheli.py
@@ -3,3 +3,12 @@ def solution():
     nums.sort(reverse=True)
     return nums[0]
 print(solution())
+
+# 2nd largest number
+
+def solut_ion():
+    nummber=[2,4,6,8,1,9,0,3]
+    nummber.sort(reverse=True)
+    return nummber[1]
+print(solut_ion())
+
